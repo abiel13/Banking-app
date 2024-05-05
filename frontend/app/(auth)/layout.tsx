7273fa85f4675cff02dc6e86b347bffdc3095a78
abiel13 <dbestabi28@gmail.com>
2024-05-05 05:31:37 +0100

    built folder architecture

---
+++ frontend/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main>
+  
+      {children}
+    </main>
+  );
+}
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans, IBM_Plex_Serif, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlex = IBM_Plex_Serif({
+  subsets: ["cyrillic"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-serif",
+});
+
+// TODO:ADD ICON
+
+export const metadata: Metadata = {
+  title: "A-Bank",
+  description: "Online Banking App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${ibmPlex.className}`}>{children}</body>
+    </html>
+  );
+}
